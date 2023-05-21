a008bf9ffb473f4e2f3cc16fcf669d977cf7648b
Emre Yoldaş <eyoldas7@gmail.com>
2023-05-21 14:48:10 +0300

    Completed

---
+++ Basamak SayilariToplama/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       int sayi,toplam=0;
+       Scanner scanner = new Scanner(System.in);
+        sayi=scanner.nextInt();
+        while (sayi != 0) {
+            toplam += sayi % 10;
+            sayi /= 10;
+        }
+        System.out.println("Sayının basamaklarının toplamı: " + toplam);
+    }
+}
